ad4b393e034cc30fa7ee4de4c061f0cdad6b422b
AWkwiecin <awkwiecin@gmail.com>
2018-11-03 02:02:56 +0100

    args

--- src/pl/awkwiecin/udemy/firstProgram/Main.java
+++ src/pl/awkwiecin/udemy/firstProgram/Main.java
@@ -3,6 +3,7 @@ package pl.awkwiecin.udemy.firstProgram;
 //project for java learning
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Intellij!");
+        System.out.println("-- JAVA LEARNING --");
+        System.out.println("Hello, " + args[0] + "!"); // You can add/switch args[] in "Edit Configurations"
     }
 }
