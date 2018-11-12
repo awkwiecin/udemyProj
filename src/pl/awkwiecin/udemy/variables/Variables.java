6a520253d30b887bfe37db995ea7bdd8a0441c7b
AWkwiecin <awkwiecin@gmail.com>
2018-11-12 20:04:24 +0100

    rest of variables

--- src/pl/awkwiecin/udemy/variables/Variables.java
+++ src/pl/awkwiecin/udemy/variables/Variables.java
@@ -2,6 +2,7 @@ package pl.awkwiecin.udemy.variables;
 
 public class Variables {
     public static void main(String[] args) {
+        System.out.println("-- Variables --");
         byte b=127;
         byte b1=-128;
         short s = Short.MAX_VALUE;
@@ -33,5 +34,51 @@ public class Variables {
         System.out.println(boo);
         System.out.println(helloHowAreYou);
         System.out.println(c);
+        //---------------------------------------------------------------------------//
+        System.out.println("-- Using variables --");
+        System.out.println("Sum: "+b+b1); //brackets matter
+        System.out.println("Sum: "+(b+b1));
+
+        int x=1+2;
+        int x1 = x+b;
+        int y = b-b1;
+        int xx=y*y;
+        int yy = x1/x;
+        double dd = x1/x;
+        double dx = 3.0;
+        double dy = 7;
+        double dz = dy/dx;
+        int r = x1%x;
+        System.out.println(x1);
+        System.out.println(yy);
+        System.out.println(dd);
+        System.out.println(dz);
+        System.out.println(r);
+        //---------------------------------------------------------------------------//
+        System.out.println("-- incrementation/decrementation --");
+        int i1=10;
+        int i2=20;
+        int i3=30;
+        int i4=40;
+        System.out.println(i1++);
+        System.out.println(i1);
+        System.out.println(++i2);
+        System.out.println(i3--);
+        System.out.println(i3);
+        System.out.println(--i4);
+        x +=5;  // x=x+5;
+        x -=5;  // x=x+5;
+        x *=5;  // x=x+5;
+        x /=5;  // x=x+5;
+        x %=5;  // x %=5;
+        System.out.println(x);
+        //---------------------------------------------------------------------------//
+
+        System.out.println("-- constants --");
+
+        final int Z_Z = 13;
+        int z1;
+        z1=8;
+        System.out.println(Z_Z);
     }
 }
