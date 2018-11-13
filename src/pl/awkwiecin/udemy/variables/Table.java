afe8e4f706a5722ab5c55a1b214970340e59477b
AWkwiecin <awkwiecin@gmail.com>
2018-11-13 01:38:16 +0100

    tables 1 dimension exercises

--- src/pl/awkwiecin/udemy/variables/Table.java
+++ src/pl/awkwiecin/udemy/variables/Table.java
@@ -5,6 +5,7 @@ import java.sql.Struct;
 public class Table {
 
     public static void main(String[] args) {
+        System.out.println("-- arrays --");
         int[] ints;
         ints = new  int[3];
         ints[0]=6;
@@ -15,5 +16,14 @@ public class Table {
 
         String[] strings = {"element", "inny element"};
         System.out.println(strings[1]);
+//-------------------------------------------------------------------------//s
+        System.out.println("-- multidimensional arrays --");
+        String[][] strings2 = {{"q", "w", "e"},{"a"},
+                {"element", "inny element","element", "inny element","element", "inny element","element", "inny element"}};
+        System.out.println(strings2[0][2]);
+        System.out.println(strings2[2][2]);
+
+        int [][][] ints3 = new  int[2][3][4];
+        ints3 = {{{}}};
     }
 }
