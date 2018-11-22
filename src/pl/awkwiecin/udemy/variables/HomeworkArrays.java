14983f23d465816ba9a2f94d440c0c0e6753b134
AWkwiecin <awkwiecin@gmail.com>
2018-11-22 02:53:14 +0100

    tables 1 dimension exercises

---
+++ src/pl/awkwiecin/udemy/variables/HomeworkArrays.java
@@ -0,0 +1,13 @@
+package pl.awkwiecin.udemy.variables;
+
+public class HomeworkArrays {
+    public static void main(String[] args) {
+        int [] homeworkArray = new int[3];
+        homeworkArray[0]=5;
+        homeworkArray[1]=12;
+        homeworkArray[2]=40;
+
+        System.out.println(homeworkArray[0]+homeworkArray[1]+homeworkArray[2]);
+        System.out.println(homeworkArray[0]*homeworkArray[1]*homeworkArray[2]);
+    }
+}
