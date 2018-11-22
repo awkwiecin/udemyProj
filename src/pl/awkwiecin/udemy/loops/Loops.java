4eed63c3019a70137ddb502cd5a73fecad5d920b
AWkwiecin <awkwiecin@gmail.com>
2018-11-22 03:16:09 +0100

    tables 1 dimension exercises

---
+++ src/pl/awkwiecin/udemy/loops/Loops.java
@@ -0,0 +1,36 @@
+package pl.awkwiecin.udemy.loops;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 30; i++) { //for (int i = 1; i <= 30; i++)
+            System.out.println("nr# " + i);
+        }
+     /* infinite loop
+        for (;;){
+            System.out.println("p");
+        }
+     */
+        int l = 20;
+        for (; l > 10; l--) {
+            System.out.println(l);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " - ");
+            for (int j = 0; j < 10; j++) {
+                System.out.print(j + ", ");
+            }
+            System.out.println("");
+        }
+
+        int [] ints = {4,3,65,123,2,90,15};
+        for (int i =0; i<ints.length; i++){
+            System.out.println(ints[i]);
+        }
+        //----foreach-----//
+        for(int i : ints) {
+            System.out.println(i);
+        }
+    }
+}
