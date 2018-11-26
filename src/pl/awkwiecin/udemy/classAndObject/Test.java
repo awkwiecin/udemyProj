9569754c71831d9e65d4b6cc0812305bdeb22ff1
AWkwiecin <awkwiecin@gmail.com>
2018-11-26 05:53:37 +0100

    Classes and objects

--- src/pl/awkwiecin/udemy/classAndObject/Test.java
+++ src/pl/awkwiecin/udemy/classAndObject/Test.java
@@ -26,5 +26,11 @@ public class Test {
         System.out.println(clientMichael.getLastName());
         System.out.println(Client.COMPANY_NAME);
         clientMichael.introduceYourself();
+        Invoice invoice1 = new Invoice("korki", 100, 5, "0002883839292", clientMichael);
+
+        System.out.println(invoice1.getBankAccountNumber());
+        System.out.println(invoice1.getTotalPrice());
+        System.out.println(invoice1.getTaxToPay());
+
     }
 }
---
+++ src/pl/awkwiecin/udemy/strings/FunWithString.java
@@ -0,0 +1,52 @@
+package pl.awkwiecin.udemy.strings;
+
+import pl.awkwiecin.udemy.classAndObject.Invoice;
+import pl.awkwiecin.udemy.utils.StrUtils;
+
+public class FunWithString {
+    public static void main(String[] args) {
+        Invoice invoice = new Invoice("korki", 100, 5, "0002883839292", null);
+
+        System.out.println(invoice.toString());
+        System.out.println(invoice);
+
+        String text = "a".concat("b");
+        String text2 = "a" + "b";
+        System.out.println(text);
+        System.out.println(text2);
+//        if (text==text2){
+//            System.out.println("equals");
+//        }
+        if (text.equals(text2)) {
+            System.out.println("equals");
+        }
+
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+
+        String s = "a,b,c,d,e";
+        String[] strings = s.split(",");
+
+        System.out.println(strings[2]);
+
+        String result = "";
+        for (String str : strings) {
+//            System.out.println(str);
+            result = result + str + ", ";
+            //System.out.println(result);
+        }
+        System.out.println(StrUtils.removeLastSeparator(result, ","));
+
+        StringBuilder sb = new StringBuilder();
+
+        String res = "";
+//        for (int i = 0; i < 1000000; i++) {
+//            res += "a";
+//        }
+//        System.out.println(res);
+        for (int i = 0; i < 1000000; i++) {
+            sb.append("a");
+        }
+        System.out.println(sb.toString());
+    }
+}
