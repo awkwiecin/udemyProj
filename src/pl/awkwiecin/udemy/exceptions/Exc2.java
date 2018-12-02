f15fc85bea36b002ce4071e8cb30f3e2aa072032
AWkwiecin <awkwiecin@gmail.com>
2018-12-02 21:17:30 +0100

    Exceptions

---
+++ src/pl/awkwiecin/udemy/exceptions/Exc2.java
@@ -0,0 +1,11 @@
+package pl.awkwiecin.udemy.exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class Exc2 {
+    public void sth() throws FileNotFoundException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(""));
+    }
+}
