4c818279a3af82712ae7e492077e920293a620b2
AWkwiecin <awkwiecin@gmail.com>
2018-12-03 01:23:54 +0100

    practise

---
+++ src/pl/awkwiecin/udemy/practise/Docu.java
@@ -0,0 +1,7 @@
+package pl.awkwiecin.udemy.practise;
+
+public class Docu {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-4));
+    }
+}
---
+++ src/pl/awkwiecin/udemy/practise/HowToDebug.java
@@ -0,0 +1,19 @@
+package pl.awkwiecin.udemy.practise;
+
+public class HowToDebug {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        String name ="Andrzej";
+
+        for (int i = 0; i <6 ; i++) {
+            if (i==4) {
+                System.out.println(i);
+            }
+            if (i==2) {
+                System.out.println("dwa");
+            }
+        }
+    }
+
+}
